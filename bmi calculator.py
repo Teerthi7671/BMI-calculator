80ac2e7d8eb3e3218904a1a99cc64a7373c4f3b7
Teerthi7671 <162325975+Teerthi7671@users.noreply.github.com>
2024-05-17 12:13:26 +0530

    Add files via upload

---
+++ bmi calculator.py
@@ -0,0 +1,26 @@
+{
+    "Vyomrajsinh": {
+        "Age": "19",
+        "Gender": "Male",
+        "BMI": 20.5,
+        "Weight Status": "Healthy Weight"
+    },
+    "Rahul": {
+        "Age": "32",
+        "Gender": "Male",
+        "BMI": 25.8,
+        "Weight Status": "Over Weight"
+    },
+    "Paramvirsinh": {
+        "Age": "19",
+        "Gender": "Male",
+        "BMI": 20.5,
+        "Weight Status": "Healthy Weight"
+    },
+    "Rohit": {
+        "Age": "23",
+        "Gender": "Male",
+        "BMI": 17.2,
+        "Weight Status": "Under weight"
+    }
+}
